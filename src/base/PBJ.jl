e3c2db61188a3bbb827513fdfb69541c4b7985b7
Calvin Lau <clau@tae.com>
2020-04-17 13:49:13 -0700

    feat:added PBJ.jl
    This is just a wrapper to call all the modules as submodules.

---
+++ src/base/PBJ.jl
@@ -0,0 +1,7 @@
+push!(LOAD_PATH, "/theory/clau/PBJ/src/base/")
+push!(LOAD_PATH, "/theory/clau/PBJ/")
+using ParticlesModule
+using GridModule
+using ParticleGridInteractionModule
+using UnitsModule
+using FieldModule
