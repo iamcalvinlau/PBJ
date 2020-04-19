d5ffa46ea876864271863dc04a95878c39c49b15
Calvin Lau <clau@TAE7147-MLAP.local>
2020-04-18 17:26:46 -1000

    feat:added 1d finite difference
    The finite difference scheme is now added for the electric field and
    Poisson solver. This is better when the domain is not periodic.

--- src/base/FieldModule.jl
+++ src/base/FieldModule.jl
@@ -2,6 +2,7 @@ module FieldModule
 
 import FFTW
 import GridModule
+using LinearAlgebra
 
 function SolvePoissonEquation_FFT_1D(
         RHS_array::Array{Float64,1},
@@ -51,11 +52,71 @@ function CalculateElectricField_FFT_1D(
     return field_out
 end
 
+function CalculateElectricField_FD_1D(
+        phi::Array{Float64,1},
+        grid_array::GridModule.Grid_Array
+    )
+    field_out = fill(0.0,length(phi))
+    dx = grid_array.x[2]-grid_array.x[1]
+    
+    for i in 2:length(phi)-1
+        field_out[i] = (phi[i+1]-phi[i-1])/(2.0*dx)
+    end
+    field_out[1]=(phi[2]-phi[1])/dx
+    field_out[end]=(phi[end]-phi[end-1])/dx
+    return field_out
+end
+
 function ApplyPeriodicBoundary!(
         field_x::Array{Float64,1}
     )
     field_x[1] += field_x[end]
     field_x[end] = field_x[1]
+end
+
+#> "grid array" type to hold grid quantities
+mutable struct Poisson_1D
+    A::LinearAlgebra.SymTridiagonal
+    phi_boundary_left::Float64
+    phi_boundary_right::Float64
+    RHS_boundary_left::Float64
+    RHS_boundary_right::Float64    
+end
+
+function Poisson_1D_Init(
+        grid_x,
+        phi_bound_left,phi_bound_right
+    )
+    dx = grid_x[2]-grid_x[1]
+    #> Note that the default length should NOT 
+    #> be the same length. The boundaries won't
+    #> be accounted for so subtract 2.
+    b = fill(+2.0/(dx*dx),length(grid_x)-2)
+    #> The upper and lower diagonals are shorted by 1.
+    a = fill(-1.0/(dx*dx),length(b)-1)
+    A_Mat = LinearAlgebra.SymTridiagonal(b,a)
+    poisson_struct = Poisson_1D(
+        A_Mat,
+        phi_bound_left,
+        phi_bound_right,
+        +phi_bound_left/(dx*dx),
+        +phi_bound_right/(dx*dx),
+    )
+    return poisson_struct
+end
+
+function Poisson_1D_Solve(
+        Poisson_struct,RHS_density
+    )
+    RHS = copy(RHS_density)
+    RHS[2] += Poisson_struct.RHS_boundary_left
+    RHS[end-1] += Poisson_struct.RHS_boundary_right
+    
+    phi = fill(0.0,length(RHS))
+    phi[1] = Poisson_struct.phi_boundary_left
+    phi[end] = Poisson_struct.phi_boundary_right
+    phi[2:end-1] = Poisson_struct.A\RHS[2:end-1];
+    return phi
 end
 
 end
