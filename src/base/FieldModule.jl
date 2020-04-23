9247c8a002bb0ccc3e12095e0d4f6385f18ce0a2
Calvin Lau <calvinlau314159@gmail.com>
2020-04-23 08:17:52 -1000

    style:change function name to be more consistent
    Consistency is important for coding style.

--- src/base/FieldModule.jl
+++ src/base/FieldModule.jl
@@ -21,31 +21,6 @@ function Fields_Init(grid)
     return fields
 end
 
-function SolvePoissonEquation_FFT_1D(
-        RHS_array::Array{Float64,1},
-        grid_array::GridModule.Grid_Array
-    )
-    #> Put the right-hand-side quantity into k-space
-    RHS_fft=FFTW.rfft(RHS_array[1:end-1])
-    
-    #> Find the wavenumber array in k-space
-    k_fft=range(0,stop=length(RHS_fft)-1,length=length(RHS_fft))
-    k_fft*=2.0*pi/(grid_array.x[end]-grid_array.x[1])
-    
-    #> k^2 (e*phi/T) = ni - ne
-    field_fft_out = RHS_fft./(k_fft.^2)
-    
-    #> For quasi-neutrality, k=0 is dropped.
-    field_fft_out[1] = 0.0+0.0im
-    #field_fft_out[3:end].*=0.0+0.0im
-    
-    #> Pull back into real-space
-    field_out=FFTW.irfft(field_fft_out,length(RHS_array[1:end-1]))
-    push!(field_out,field_out[1])
-    
-    return field_out
-end
-
 function CalculateElectricField_FFT_1D(
         phi::Array{Float64,1},
         grid_array::GridModule.Grid_Array
@@ -150,6 +125,30 @@ function Poisson_1D_Solve(
     phi[end] = Poisson_struct.phi_boundary_right
     phi[2:end-1] = Poisson_struct.A\RHS[2:end-1];
     return phi
+end
+
+function Poisson_1D_Solve_Periodic(
+        RHS_array::Array{Float64,1},
+        grid_array::GridModule.Grid_Array
+    )
+    #> Put the right-hand-side quantity into k-space
+    RHS_fft=FFTW.rfft(RHS_array[1:end-1])
+    
+    #> Find the wavenumber array in k-space
+    k_fft=range(0,stop=length(RHS_fft)-1,length=length(RHS_fft))
+    k_fft*=2.0*pi/(grid_array.x[end]-grid_array.x[1])
+    
+    #> k^2 (e*phi/T) = ni - ne
+    field_fft_out = RHS_fft./(k_fft.^2)
+    
+    #> For quasi-neutrality, k=0 is dropped.
+    field_fft_out[1] = 0.0+0.0im
+    
+    #> Pull back into real-space
+    field_out=FFTW.irfft(field_fft_out,length(RHS_array[1:end-1]))
+    push!(field_out,field_out[1])
+    
+    return field_out
 end
 
 end
